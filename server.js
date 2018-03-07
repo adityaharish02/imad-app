6fb6ff96b891d7bedbc33430f899da2ab9aeb1ff
adityaharish02 <36709811+adityaharish02@users.noreply.github.com>
2018-03-07 18:14:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -107,9 +107,23 @@ app.get('/submit-name', function(req, res){
     res.send(JSON.stringify(names));
 });
 
-app.get('/:articleName', function(req, res){
-    var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+app.get('/article/:articleName', function(req, res){
+    //var articleName = req.params.articleName;
+    //res.send(createTemplate(articles[articleName]));
+    pool.query("SELECT * from articles where title = " + req.params.articleName, function(err, result){
+        if (err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            if(result.rows.length === 0){
+                res.status(404).send('Article not found');
+            }
+            else{
+                var articleData = result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+    });
 });
 
 
