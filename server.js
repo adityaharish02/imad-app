6cd175a06a71e6ac9cb9d2b4082d56c61e87c82a
adityaharish02 <36709811+adityaharish02@users.noreply.github.com>
2018-03-08 11:21:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -125,7 +125,7 @@ app.post('/login', function(req, res){
                var dbString = result.rows[0].password;
                var salt = dbString.split('$')[2];
                var hashedPassword = hash(password, salt);
-               if(hashedPassword === password){
+               if(hashedPassword === dbString){
                    res.send('Credentials are correct');
                }
                else{
