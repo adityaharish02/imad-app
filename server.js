f10f3722c4a7ace64bd35fa496b613a0bce826b6
adityaharish02 <36709811+adityaharish02@users.noreply.github.com>
2018-03-08 11:17:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -112,8 +112,7 @@ app.post('/create-user', function(req, res){
 app.post('/login', function(req, res){
     var username = req.body.username;
     var password = req.body.password;
-    
-    var dbString = hash(password, salt);
+
     pool.query('SELECT * FROM users WHERE username = $1', [username], function(err, result){
       if(err){
            res.status(500).send(err.toString());
