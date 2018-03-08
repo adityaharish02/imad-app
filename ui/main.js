afdfc46c61ea966c9cca763ea726a7c0c71d575e
adityaharish02 <36709811+adityaharish02@users.noreply.github.com>
2018-03-08 11:53:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,7 +15,7 @@ img.onclick = function(){
 };
 */
 
-var button = document.getElementById('counter');
+/* var button = document.getElementById('counter');
 
 button.onclick = function(){
     
@@ -35,8 +35,10 @@ button.onclick = function(){
     request.send(null);
     
 };
+*/
 
 
+/*
 var submit = document.getElementById('submit_btn');
 
 submit.onclick = function(){
@@ -62,4 +64,33 @@ submit.onclick = function(){
     var name = nameInput.value;
     request.open('GET','http://adityaharish04.imad.hasura-app.io/submit-name?name=' + name,true);
     request.send(null);
+} ;
+*/
+var submit = document.getElementById('submit_btn');
+
+submit.onclick = function(){
+    
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+      if(request.readyState === XMLHttpRequest.DONE){
+          if (request.status === 200){
+            alert('Logged in successfully !!');
+        }
+        else if(request.status === 403){
+            alert('Username/password is incorrect');
+        }
+        else if(request.status === 500){
+            alert('Something went wrong on the server.');
+        }
+      } 
+    };
+    
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST','http://adityaharish04.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username: username, password: password}));
 } ;
